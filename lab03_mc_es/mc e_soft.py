2e8f0328b84b22a0d2196fc110e3eb51d412a5e9
nyeonsoo <nys827827@gmail.com>
2025-02-20 15:33:26 +0900

    lab03 commit

--- lab03_mc_es/mc e_soft.py
+++ lab03_mc_es/mc e_soft.py
@@ -13,33 +13,28 @@ def state_to_index(state, n):
     row, col = player_pos[0]
     return row * n + col
 
-def generate_episode_with_exploring_starts(env, policy, trajectory_length, Q, episode_index):
+def epsilon_greedy_policy(Q, state_idx, epsilon, num_actions):
+    """ε-greedy policy: choose a random action with probability ε, otherwise the greedy action"""
+    if np.random.rand() < epsilon:
+        return np.random.choice(num_actions)  # Explore (random action)
+    else:
+        return np.argmax(Q[state_idx])  # Exploit (best action)
 
-    # Reset environment with new initial player position
+def generate_episode(env, policy, trajectory_length, epsilon, Q, episode_index):
+
+    # Reset the environment
     state, _ = env.retry()
-    all_states = env.get_all_states()            
-    random_player_pos = all_states[np.random.choice(len(all_states))]
-    env.set_player_pos(random_player_pos)
 
-
-    # Generate an episode with exploring starts
     trajectory = []
     state_action_pairs = []
 
-    state = env._get_state()
     for _ in range(trajectory_length):  # Limiting the trajectory length
         state_idx = state_to_index(state, env.n)
-        if state_idx is None: # the player is at either pit or goal. 
+        if state_idx is None:  # The player is at either the pit or goal
             break
 
-        # Record the state-action pair and its reward
-        if len(trajectory) == 0: # If this is the first state-action pair
-            # Select a random action for exploring starts (even if policy exists)
-            action = np.random.choice(env.action_space.n)    
-        else:            
-            # Select an action probabilistically based on the policy
-            action = np.random.choice(env.action_space.n, p=policy[state_idx])            
-
+        # Select an action using the ε-greedy policy
+        action = epsilon_greedy_policy(Q, state_idx, epsilon, env.action_space.n)
 
         next_state, reward, terminated, _ = env.step(action)
         trajectory.append((state_idx, action, reward))
@@ -53,20 +48,18 @@ def generate_episode_with_exploring_starts(env, policy, trajectory_length, Q, ep
     return trajectory, state_action_pairs
 
 
-def monte_carlo_es(env, gamma=0.99, num_episodes=10000, trajectory_length = 100):
+def monte_carlo_e_soft(env, gamma, num_episodes, trajectory_length, epsilon):
     policy = np.ones((env.n * env.n, env.action_space.n)) / env.action_space.n
     Q = np.zeros((env.n * env.n, env.action_space.n))
     returns = {state: {action: [] for action in range(env.action_space.n)} for state in range(env.n * env.n)}
 
     for episode_index in range(num_episodes):
-
-        # Generate an episode with exploring starts (random initial state and action)
-        trajectory, visited_state_action_pairs = generate_episode_with_exploring_starts(env, policy, trajectory_length, Q, episode_index)
+        # Generate an episode with ε-soft action selection
+        trajectory, visited_state_action_pairs = generate_episode(env, policy, trajectory_length, epsilon, Q, episode_index)
 
         G = 0  # Initialize return value
 
         for t in reversed(range(len(trajectory))):
-
             state_idx, action, reward = trajectory[t]
             G = gamma * G + reward  # Accumulate discounted rewards
 
@@ -74,14 +67,18 @@ def monte_carlo_es(env, gamma=0.99, num_episodes=10000, trajectory_length = 100)
             if (state_idx, action) not in visited_state_action_pairs[:t]:
                 # Append G to the list of returns for (state, action)
                 returns[state_idx][action].append(G)
-                
+
                 # Update Q(s, a) with the average of returns for this (state, action) pair
                 Q[state_idx][action] = np.mean(returns[state_idx][action])
 
-                # Improve the policy to be greedy with respect to Q
+                # Improve the policy to be ε-soft with respect to Q
                 best_action = np.argmax(Q[state_idx])
-                policy[state_idx] = np.eye(env.action_space.n)[best_action]  # Set the best action with probability 1
-
+                for a in range(env.action_space.n):
+                    if a == best_action:
+                        policy[state_idx][a] = 1 - epsilon + (epsilon / env.action_space.n)
+                    else:
+                        policy[state_idx][a] = epsilon / env.action_space.n
+    
     return policy, Q
 
 def main():
@@ -98,18 +95,19 @@ def main():
     # random_seed = 2
 
     gamma = 0.9
+    epsilon = 0.1
     num_episodes = 1000
-    trajectory_length = n*5
+    trajectory_length = n * 5
 
     env = SimpleMazeGrid(n=n, k=k, m=m, render_option=True, random_seed=random_seed)
-    policy, Q = monte_carlo_es(env, gamma=gamma, num_episodes=num_episodes, trajectory_length=trajectory_length)
+    policy, Q = monte_carlo_e_soft(env, gamma=gamma, num_episodes=num_episodes, trajectory_length=trajectory_length, epsilon=epsilon)
 
     # Plot the policy or Q-values
     env.render_q_values(Q, num_episodes, with_arrow=True)
     plt.imshow(np.max(Q, axis=1).reshape((n, n)), cmap='hot', interpolation='nearest')
     plt.colorbar()
     plt.title('State-Action Values (Q)')
-    plt.savefig('monte_carlo_es_simple_maze_grid.png')
+    plt.savefig('monte_carlo_e_soft_simple_maze_grid.png')
     plt.show()
 
     print("Completed!")
