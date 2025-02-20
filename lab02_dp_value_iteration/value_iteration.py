3a32bd14be45ff897efcd9072fea5fdf04e0dc9d
nyeonsoo <nys827827@gmail.com>
2025-02-20 15:20:48 +0900

    value iteration commit

--- lab02_dp_value_iteration/value_iteration.py
+++ lab02_dp_value_iteration/value_iteration.py
@@ -31,23 +31,36 @@ def value_iteration(env, gamma=0.99, theta=1e-6):
             v = V[state_idx]
 
             # NOTE: The following can be used to visualise this algorithm's process
-            # if env.render_option: 
-            #     env.render_v_values(V, policy, iteration)     
+            if env.render_option: 
+                env.render_v_values(V, policy, iteration)
 
-            # TODO: Implement the value iteration algorithm            
+            # Value iteration update: simultaneously update V(s) by finding the maximum value over actions
+            q_values = np.zeros(env.action_space.n)
+            for action in range(env.action_space.n):
+                next_state, reward, terminated = env.simulate_action(player_pos, action)[:3]
+                next_state_idx = state_to_index(next_state, env.n)
+                if next_state_idx is not None:
+                    q_values[action] = reward + gamma * V[next_state_idx] * (not terminated)
+                else:
+                    q_values[action] = reward
+
+            # Update value function for the current state
+            V[state_idx] = np.max(q_values)
+            delta = max(delta, abs(v - V[state_idx]))
                     
-
-
+            best_action = np.argmax(q_values)
+            policy[state_idx] = np.eye(env.action_space.n)[best_action]   # Set the best action as the greedy policy
+        
         iteration += 1
-     
 
     return policy, V, iteration
 
+
 def main():
-    n = 5
-    k = 3
-    m = 4
-    random_seed = 42
+    n = 10
+    k = 9
+    m = 20
+    random_seed = 2
     gamma = 0.9
     theta = 1e-6
 
